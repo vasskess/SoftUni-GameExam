f4334ed78cd1f0b420287048c698a30e905c6505
Vasil <vrangelov85@abv.bg>
2022-10-28 07:45:27 +0300

    fixed my_game folder

--- game/my_game/urls.py
+++ game/my_game/urls.py
@@ -3,7 +3,7 @@ from game.my_game.views import *
 
 urlpatterns = (
     path("create/", create_game, name="create-game"),
-    path("details/<id>/", game_details, name="details-game"),
-    path("edit/<id>/", edit_game, name="edit-game"),
-    path("delete/<id>/", delete_game, name="delete-game"),
+    path("details/<pk>/", game_details, name="details-game"),
+    path("edit/<pk>/", edit_game, name="edit-game"),
+    path("delete/<pk>/", delete_game, name="delete-game"),
 )
--- game/my_game/views.py
+++ game/my_game/views.py
@@ -1,51 +1,50 @@
 from django.shortcuts import render, redirect
 
-from game.my_game.forms import GameForm, DeleteGameForm
+from game.my_game.forms import CreateGameForm, EditGameForm, DeleteGameForm
 from game.my_game.models import Game
 
 
-# Create your views here.
-
-
 def create_game(request):
-    form = GameForm()
+    form = CreateGameForm()
 
     if request.method == "POST":
-        form = GameForm(request.POST)
+        form = CreateGameForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("dashboard")
     context = {"form": form}
-    return render(request, "my_game/create-game.html", context)
+    return render(request, template_name="my_game/create-game.html", context=context)
 
 
-def game_details(request, id):
-    game = Game.objects.get(id=id)
+def game_details(request, pk):
+    game = Game.objects.get(pk=pk)
     context = {"game": game}
 
-    return render(request, "my_game/details-game.html", context)
+    return render(request, template_name="my_game/details-game.html", context=context)
 
 
-def edit_game(request, id):
-    game = Game.objects.get(id=id)
-    form = GameForm(instance=game)
+def edit_game(request, pk):
+    game = Game.objects.get(pk=pk)
+    form = EditGameForm(instance=game)
 
     if request.method == "POST":
-        form = GameForm(request.POST, instance=game)
+        form = EditGameForm(request.POST, instance=game)
         if form.is_valid():
             form.save()
             return redirect("dashboard")
 
     context = {"game": game, "form": form}
-    return render(request, "my_game/edit-game.html", context)
+    return render(request, template_name="my_game/edit-game.html", context=context)
 
 
-def delete_game(request, id):
-    game = Game.objects.get(id=id)
+def delete_game(request, pk):
+    game = Game.objects.get(pk=pk)
     form = DeleteGameForm(instance=game)
 
     if request.method == "POST":
-        game.delete()
+        form = DeleteGameForm(request.POST, instance=game)
+        if form.is_valid():
+            form.save()
         return redirect("dashboard")
     context = {"game": game, "form": form}
-    return render(request, "my_game/delete-game.html", context)
+    return render(request, template_name="my_game/delete-game.html", context=context)
